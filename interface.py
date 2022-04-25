450d47ff7f63428f9265cfa61590f94d5a779b8a
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-04-25 03:08:00 +0300

    add display credentials method and test for it

--- interface.py
+++ interface.py
@@ -23,7 +23,7 @@ class TestClass(unittest.TestCase):
                      """
                      def test_init(self):
                          self.assertEqual(self.new_credential.account,'Gmail')
-                         self.assertEqual(self.new_credential.userName,'Owiti_Charles')
+                         self.assertEqual(self.new_credential.userName,'moglie02')
                          self.assertEqual(self.new_credential.password,'yx5Gij43')
                          
                      def save_credential_test(self):
